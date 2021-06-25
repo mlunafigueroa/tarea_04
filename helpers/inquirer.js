1a6f5ace28de2dcab914214069b548e10b2e83b2
mlunafigueroa <luna_figueroa@yahoo.com.ar>
2021-06-25 06:20:36 -0300

    correccion colors

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -7,36 +7,37 @@ const preguntas = [{
     message: 'Que desea hacer?',
     choices: [{
             value: '1',
-            name: `{'1.'.green} Crear Tarea`
+            name: `${'1.'.green} Crear Tarea`
         },
         {
             value: '2',
-            name: `{'2.'.green} Listar Tareas`
+            name: `${'2.'.green} Listar Tareas`
         },
         {
             value: '3',
-            name: `{'3.'.green} Listar Tareas Completadas`
+            name: `${'3.'.green} Listar Tareas Completadas`
         },
         {
             value: '4',
-            name: `{'4.'.green} Listar Tareas Pendientes`
+            name: `${'4.'.green} Listar Tareas Pendientes`
         },
         {
             value: '5',
-            name: `{'5.'.green} Completar Tarea(s)`
+            name: `${'5.'.green} Completar Tarea(s)`
         },
         {
             value: '6',
-            name: `{'6.'.green} Borrar Tarea`
+            name: `${'6.'.green} Borrar Tarea`
         },
         {
             value: '0',
-            name: `{'0.'.green} Salir`
+            name: `${'0.'.green} Salir`
         },
     ]
 }];
 
 const inquirerMenu = async() => {
+
     console.clear();
     console.log('================================='.green);
     console.log('Seleccionar Opción'.white);
@@ -51,7 +52,7 @@ const pausa = async() => {
     const question = [{
         type: 'input',
         name: 'enter',
-        message: `Presione $ { 'ENTER'.green} para continuar`
+        message: `Presione ${'ENTER'.green} para continuar`
     }];
     await inquirer.prompt(question);
 };
